6d406c99976e410ea766abaaded64c6f97e3cf25
Nobuo Kato <katonobu4649@gmail.com>
2026-06-29 13:31:12 +0900

    dirlist.jsonを更新する

---
+++ util/generate_directory_list.py
@@ -0,0 +1,41 @@
+import boto3
+import json
+import os
+
+if os.getenv("GITHUB_ACTIONS", "false").lower() == "true":
+    BUCKET = os.environ.get("BUCKET")
+    PREFIX = os.environ.get("PREFIX")
+    s3 = boto3.client("s3")
+else:
+    BUCKET="frontend-cmn-weatherchart-content"
+    PREFIX="shared/services/weatherchart/"
+
+    session = boto3.Session(profile_name="cnt")
+    s3 = session.client("s3")
+
+response = s3.list_objects_v2(
+    Bucket=BUCKET,
+    Prefix=PREFIX,
+    Delimiter="/"
+)
+
+dirs = []
+
+for item in response.get("CommonPrefixes", []):
+    prefix = item["Prefix"]
+
+    # prefix削除
+    name = prefix.replace(PREFIX, "", 1)
+
+    # 末尾スラッシュ削除
+    name = name.rstrip("/")
+
+    if name:
+        dirs.append(name)
+
+# JSON出力
+with open("directory_list.json", "w") as f:
+    json.dump(dirs, f, indent=2)
+
+print("Generated directory_list.json")
+print(json.dumps(dirs, indent=2))
